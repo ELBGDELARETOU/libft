99b2e2fc2eb02f41ed9a10ea7f946ab9d12f5ad8
Adem Naouali <anaouali@bess-f3r6s7.clusters.42paris.fr>
2023-11-09 15:46:22 +0100

    99

---
+++ ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anaouali <anaouali@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 15:25:14 by anaouali          #+#    #+#             */
+/*   Updated: 2023/11/09 15:45:53 by anaouali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*strdup(const char *str)
+{
+	size_t size;
+	size_t i;
+	unsigned char dest;
+
+	i = 0;
+	size = 0;
+	size = ft_strlen(str);
+	dest = (char *)malloc(size + 1);
+	if (!dest)
+		return (NULL);
+	while (taill > i)
+	{
+		dest[i] = str[i];
+		i++;
+		size--;
+	}
+	dest[i] = '\0';
+	return (dest)
+}
